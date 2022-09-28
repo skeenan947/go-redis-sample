77eb6cc323600bf4edefaa63a2e77c0bb005286e
Pablo Russo <prusso@zencore.dev>
2022-09-28 16:19:35 -0300

    adding helm push / helm install

--- module/sandbox/helm.tf
+++ module/sandbox/helm.tf
@@ -1,15 +1,13 @@
-resource "null_resource" "helm_login" {
-  provisioner "local-exec" {
-    command = "gcloud auth print-access-token | helm registry login -u oauth2accesstoken --password-stdin https://${var.region}-docker.pkg.dev"
-  }
-}
-
 resource "helm_release" "go-redis" {
-  chart      = "go-redis"
-  repository = "oci://${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.samples.repository_id}"
-  name       = "go-redis"
+  name = "go-redis"
+  #chart = "oci://us-west1-docker.pkg.dev/go-redis/samples/go-redis"
+  #chart = "../../chart/go-redis/go-redis-0.1.0.tgz"
+  chart            = "oci://${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.samples.repository_id}/go-redis"
+  namespace        = "go-redis"
+  create_namespace = true
 
   depends_on = [
-    null_resource.helm_login
+    null_resource.helm_push,
+    module.gke
   ]
 }
--- module/sandbox/provider.tf
+++ module/sandbox/provider.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.1.0"
+    }
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = var.region
@@ -10,4 +24,13 @@ provider "kubernetes" {
   host                   = "https://${module.gke.endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "null" {
+  # Configuration options
 }
